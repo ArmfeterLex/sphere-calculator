89af4e2be707bba12ef41c8d750168e2ff680084
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-14 22:33:15 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите радиус шара");
+        double a = double.Parse(Console.ReadLine());
+        double s = Math.Round(4 * Math.PI * (a * a), 2); double v = Math.Round((4 * Math.PI * (a * a * a)) / 3, 2);
+        Console.WriteLine("Площадь шара {0:f2}; Обьем шара {1:f2}", s, v);
+        Console.ReadKey();
+    }
+}
